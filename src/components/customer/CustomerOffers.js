c788003ecf00ce365025e1afce4f860fbbe5b40c
Kaleb Wondwossen <kaleb.wond@gmail.com>
2023-04-28 15:06:57 -0500

    download offers by customer

--- src/components/customer/CustomerOffers.js
+++ src/components/customer/CustomerOffers.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import userAxios from '../../util/axios'
 import { UserContext } from '../../context/UserContext'
-
+import { ArrowDownOnSquareStackIcon } from '@heroicons/react/24/outline'
 
 function CustomerOffers() {
   const [offers, setOffers] = useState([])
@@ -45,11 +45,49 @@ function CustomerOffers() {
         break;
     }
   }
+// vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8
+  const onDownload = () => {
+    let csvRows = []
+    let data = offers.map(o => {
+      return {
+        id: o.id,
+        price: o.price,
+        status: o.status,
+        submittedAt: o.submittedAt,
+        propertyPrice: o.property.price,
+        propertyStatus: o.property.propertyStatus,
+        propertyType: o.property.propertyType,
+      }
+    })
+    const header = ["Id", "Offer Price", "Offer Status", "Offer Date", "Property Price", "Property Status", "Property Type"]
+    data = data.map(dt => Object.values(dt).join(","))
+    csvRows.push(header.join(','))
+    csvRows = [...csvRows, ...data]
+    data = csvRows.join('\n')
+    // file object
+    const file = new Blob([data], { type: 'application/csv' });
+
+    // anchor link
+    const element = document.createElement("a");
+    element.href = URL.createObjectURL(file);
+    element.download = "100ideas-" + Date.now() + ".csv";
+
+    // simulate link click
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
 
   return (
     <div className="flex flex-col">
-      <h1 className="text-xl font-medium">My Offers</h1>
-
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-medium">My Offers</h1>
+        {offers.length > 0 && (
+          <button onClick={onDownload} className="border rounded px-3 py-1.5 flex items-center hover:border-gray-300 hover:shadow-sm transition">
+            Download
+            <ArrowDownOnSquareStackIcon className="h-6 w-6 ml-2" />
+          </button>
+        )}
+      </div>
 
       <div className="mt-8 relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left text-gray-600 dark:text-gray-400">
